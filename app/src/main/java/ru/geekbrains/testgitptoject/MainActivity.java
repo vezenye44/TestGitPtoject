cbfcf492deb8978e84c02218aeef61e19a684f10
Alecsey Efimov <alicsey2000@gmail.com>
2022-06-21 15:59:22 +0300

    start project

---
+++ app/src/main/java/ru/geekbrains/testgitptoject/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.testgitptoject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // master's d
+    }
+}
